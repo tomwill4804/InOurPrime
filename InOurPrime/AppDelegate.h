a2ecce55193bbc2c04784e3950813121955b5ad3
Tom Williamson <tom@dtssoftware.com>
2016-04-27 13:24:27 -0400

    Initial Commit

---
+++ InOurPrime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InOurPrime
+//
+//  Created by Tom Williamson on 4/27/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InOurPrime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InOurPrime
+//
+//  Created by Tom Williamson on 4/27/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
